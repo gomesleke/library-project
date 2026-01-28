9378c63057bab77ae708acd959de437da2dde242
gomesleke <voyagerleke@gmail.com>
2026-01-28 20:19:56 -0300

    start project

---
+++ readme.md
@@ -0,0 +1,3 @@
+This File Project have object to learn python to backend
+
+make a library
---
+++ skull/back_idea/main.py
@@ -0,0 +1,19 @@
+# make a idea about the libray
+'''
+librry have section books and new books
+'''
+
+import os
+
+print('hello world')
+
+
+
+
+
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
